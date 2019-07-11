6a1eb751316bdf26f99522e0acb7edf0647b42e1
Washington Añasco <wacho_jax@hotmail.com>
2019-07-11 00:16:00 -0500

    Menu de navegacion
    Navegacion del menu principal de la app.

--- app/src/main/java/com/example/dr/turis_huma/MainActivity.kt
+++ app/src/main/java/com/example/dr/turis_huma/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.dr.turis_huma
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_menuPrincipal.setOnClickListener {
+            val Intent = Intent(this,AppMenuActivity::class.java)
+            startActivity(Intent)
+        }
     }
 }
