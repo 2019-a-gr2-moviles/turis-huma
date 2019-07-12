003f154107f7b40104ceb8ade646061aa0ebb642
Jacinto Palma <jxcinto12@gmail.com>
2019-07-12 13:13:01 -0500

    Register - Authentication Firebase

---
+++ app/src/main/java/com/example/dr/turis_huma/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.dr.turis_huma
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        register_link.setOnClickListener {
+           registerNewUser()
+        }
+
+    }
+
+    fun registerNewUser(){
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
+    }
+}
