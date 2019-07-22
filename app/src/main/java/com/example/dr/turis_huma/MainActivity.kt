5938dd8fe8b866b0c8729a0a633cc814fd534843
Jacinto Palma <jxcinto12@gmail.com>
2019-07-22 11:31:46 -0500

    Logout
    Allows to exit the app , and excecute an intent to LoginActivity

--- app/src/main/java/com/example/dr/turis_huma/MainActivity.kt
+++ app/src/main/java/com/example/dr/turis_huma/MainActivity.kt
@@ -3,6 +3,12 @@ package com.example.dr.turis_huma
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.widget.RecyclerView
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -10,6 +16,9 @@ class MainActivity : AppCompatActivity() {
 
 
     private val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    private var navigationView: NavigationView? = null
+    private var drawerLayout: DrawerLayout? = null
+    private var recyclerView: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +28,38 @@ class MainActivity : AppCompatActivity() {
         if(currentUser == null){
             sendUserToLoginActivity()
         }
+
+
+        drawerLayout = findViewById<View>(R.id.drawable_layout) as DrawerLayout?
+        navigationView = findViewById<View>(R.id.navigation_view) as NavigationView?
+
+
+        navigationView!!.setNavigationItemSelectedListener { menuItem ->
+            UserMenuSelector(menuItem)
+            false
+        }
+
+
     }
 
     fun sendUserToLoginActivity(){
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun UserMenuSelector(item: MenuItem) {
+
+        when (item.itemId) {
+            R.id.home -> {
+                Toast.makeText(this, "Pantalla de Inicio", Toast.LENGTH_SHORT).show()
+
+            }
+
+            R.id.logout -> {
+                mAuth.signOut()
+                sendUserToLoginActivity()
+                Toast.makeText(this, "Salir", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
